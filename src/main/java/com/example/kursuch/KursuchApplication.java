3f9ad254f64f74b83a6b2f151311133b11d9f6f7
RedArmyIsStrongestAll <redarmyisstrongestall@yandex.ru>
2022-06-01 17:50:44 +0300

    start

---
+++ src/main/java/com/example/kursuch/KursuchApplication.java
@@ -0,0 +1,13 @@
+package com.example.kursuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursuchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KursuchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kursuch/KursuchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kursuch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursuchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
